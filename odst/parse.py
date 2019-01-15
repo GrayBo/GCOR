0de6161a70c1a4473d0de57cf106ccc1948a47db
GrayBo <graysonelliott.boese@calbaptist.edu>
2019-01-15 15:27:51 -0800

    parser

--- odst/parse.py
+++ odst/parse.py
@@ -4,26 +4,29 @@ from nltk.parse.corenlp import CoreNLPParser
 class Parser:
 
 	class __Parser:
-        """ Implementation of the singleton class Parser """
-        def __init__(self):
-            self.parser = CoreNLPParser()
-        	self.parser.session.trust_env = False
+
+		def __init__(self):
+			self.parser = CoreNLPParser()
+			self.parser.session.trust_env = False
+
+		def parse(self, sentence):
+			return self.parser.raw_parse(sentence)
 
     # storage for the instance reference
-    __instance = None
+	__instance = None
     
-    def __init__(self):
+	def __init__(self):
     	# Check whether we already have an instance
-        if Parser.__instance is None:
+		if Parser.__instance is None:
             # Create and remember instance
-            Parser.__instance = Parser.__Parser()
+			Parser.__instance = Parser.__Parser()
           
         # Store instance reference as the only member in the handle
-        self.__dict__['_Parser__instance'] = Parser.__instance
+		self.__dict__['_Parser__instance'] = Parser.__instance
         
 
-    def parse(self, sentence):
-        return self.parser.raw_parse(sentence)
+	def parse(self, sentence):
+		return Parser.__instance.parse(sentence)
 
 
 if __name__ == '__main__':
